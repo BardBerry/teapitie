2c23747f82a3a4d5b5b939c9eea9a74f4396e4b3
BardBerry <bardberry@yandex.ru>
2022-07-08 11:35:23 +0300

    fixes3

--- routes/lk.router.js
+++ routes/lk.router.js
@@ -4,11 +4,16 @@ const { Tea, User, Comment } = require('../db/models');
 router
   .route('/')
   .get(async (req, res) => {
-    const response = await fetch('http://localhost:3000/lk/all');
-    if (response.ok) {
-      const { data, comment } = await response.json();
-      console.log({ comment });
+    try {
+      const data = await Tea.findAll();
+      const comment = await Comment.findAll({
+        include: {
+          model: User,
+        },
+      });
       return res.render('adminlk', { data, comment });
+    } catch (error) {
+      res.sendStatus(500);
     }
   })
 
@@ -16,7 +21,6 @@ router
     const {
       title, description, location, img,
     } = req.body;
-    // console.log(title);
     const addTea = Tea.create({
       // eslint-disable-next-line max-len
       title, description, location, img, latitude: null, longitude: null, createdAt: new Date(), updatedAt: new Date(),
